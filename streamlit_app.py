2cffd6f93877eb5c5064077972ec960b3aee6768
Iftekhar Mahmud <mahmudtarif@gmail.com>
2025-08-10 12:30:05 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ from geopy.geocoders import Nominatim
 import pydeck as pdk
 
 # Load CSV data
-data = pd.read_csv('https://raw.githubusercontent.com/iftekhar-mahmud/Crop-Price-Prediction/refs/heads/master/Data/CombinedDataset.csv')
+data = pd.read_csv('https://raw.githubusercontent.com/iftekhar-mahmud/Crop-Price-Prediction/refs/heads/master/Data/Updated Combined Dataset.csv')
 
 # Clean & preprocess
 data = data.dropna()
